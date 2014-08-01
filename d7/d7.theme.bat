39003b46b808ed89e06a2dd9bcdb3e0dca4fa780
Xuan Ngo <xuanngo@users.noreply.github.com>
2014-08-01 12:58:45 -0400

    Fix paths.

--- d7/d7.theme.bat
+++ d7/d7.theme.bat
@@ -1,39 +1,26 @@
-SET drupal_root=C:\temp\Xuan\wamp\www\openwritings.net
-SET module_dir=%drupal_root%\sites\all\modules
-SET theme_dir=%drupal_root%\sites\all\themes
-SET library_dir=%drupal_root%\sites\all\libraries
-SET d7modules_dir=archive\d7modules
-SET temp_color=%drupal_root%\sites\default\files\color
-SET temp_css=%drupal_root%\sites\default\files\css
-SET temp_js=%drupal_root%\sites\default\files\js
+SET drupal_root_dir=C:\temp\Xuan\wamp\www\openwritings.net
+SET module_dir=%drupal_root_dir%\sites\all\modules
+SET theme_dir=%drupal_root_dir%\sites\all\themes
+SET library_dir=%drupal_root_dir%\sites\all\libraries
+
+SET temp_color=%drupal_root_dir%\sites\default\files\color
+SET temp_css=%drupal_root_dir%\sites\default\files\css
+SET temp_js=%drupal_root_dir%\sites\default\files\js
 
 rd /q /s %temp_color%
 rd /q /s %temp_css%
 rd /q /s %temp_js%
 
 
-REM rd /q /s %theme_dir%\owth_garland
-REM mkdir %theme_dir%\owth_garland
-REM xcopy /s/h/e %d7modules_dir%\owth_garland %theme_dir%\owth_garland\
-REM CALL drush.bat -r %drupal_root% pm-enable -y owth_garland
-REM CALL drush.bat -r %drupal_root% vset -y theme_default owth_garland
-
-
-REM rd /q /s %theme_dir%\owth_bartik
-REM mkdir %theme_dir%\owth_bartik
-REM xcopy /s/h/e %d7modules_dir%\owth_bartik %theme_dir%\owth_bartik\
-REM CALL drush.bat -r %drupal_root% pm-enable -y owth_bartik
-REM CALL drush.bat -r %drupal_root% vset -y theme_default owth_bartik
-
 
 rd /q /s %theme_dir%\antonelli
 mkdir %theme_dir%\antonelli
-xcopy /s/h/e %d7modules_dir%\antonelli %theme_dir%\antonelli\
-CALL drush.bat -r %drupal_root% pm-enable -y antonelli
-CALL drush.bat -r %drupal_root% vset -y theme_default antonelli
+xcopy /s/h/e antonelli %theme_dir%\antonelli\
+CALL drush.bat -r %drupal_root_dir% pm-enable -y antonelli
+CALL drush.bat -r %drupal_root_dir% vset -y theme_default antonelli
 
 
 REM =========================================================================================================
-CALL drush.bat -r %drupal_root% cc theme-registry
-CALL drush.bat -r %drupal_root% cache-clear all
-CALL drush.bat -r %drupal_root% cron
+CALL drush.bat -r %drupal_root_dir% cc theme-registry
+CALL drush.bat -r %drupal_root_dir% cache-clear all
+CALL drush.bat -r %drupal_root_dir% cron
